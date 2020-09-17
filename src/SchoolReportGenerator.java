bc2812595a05b2873df4e91f59da2aad2846c00d
Sophie <sophiebrown438@gmail.com>
2020-09-17 12:23:40 +0100

    Pass test for multiple Green grades

--- src/SchoolReportGenerator.java
+++ src/SchoolReportGenerator.java
@@ -1,11 +1,22 @@
 public class SchoolReportGenerator {
     public String write(String scores) {
 
-        Integer count = 1;
-        if (scores.equals("Green, Green")) {
+        Integer count = 0;
+        if(scores.equals("Green, Green")){
+            count += 2;
+            scores = "Green";
+        } else if (scores.equals("Green")) {
             count ++;
             scores = "Green";
         }
+        else if (scores.contains("Amber")) {
+            count ++;
+            scores = "Amber";
+        } else if (scores.contains("Red")) {
+            count ++;
+            scores = "Red";
+        }
+
         String output = String.format(": %s", count);
         return scores + output;
     }
